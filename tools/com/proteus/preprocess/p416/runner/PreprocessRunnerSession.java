8d1ed2931c31b1ffa599d6e8c1b4187e232ed0d7
sureshgl <suresh.goduguluru@gmail.com>
2017-04-03 11:39:41 +0530

    removing the empty lines

--- tools/com/proteus/preprocess/p416/runner/PreprocessRunnerSession.java
+++ tools/com/proteus/preprocess/p416/runner/PreprocessRunnerSession.java
@@ -7,14 +7,12 @@ import java.util.List;
 
 import lombok.Data;
 
-import org.antlr.v4.runtime.ParserRuleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.proteus.common.util.FileUtils;
 import com.proteus.preprocess.pp.ext.Macro;
 import com.proteus.preprocess.pp.ext.MacroKey;
-import com.proteus.preprocess.pp.ext.ProgramContextExt;
 import com.proteus.preprocess.pp.gen.P416PPParser.ProgramContext;
 import com.proteus.preprocess.pp.gen.P416PPParser.StatementContext;
 
@@ -53,15 +51,34 @@ public class PreprocessRunnerSession {
 		}
 		ProgramContext newContext = fp.getStartContext(sb.toString());
 		writeToFile(newContext.extendedContext.getFormattedText(),this.getProcessingFile().getName());
+		cleanProcessedFile();
 		return pc;
+	}
+
+
+	private void cleanProcessedFile(){
+		File outputFile = new File(this.getOutputDir()+"/"+this.getProcessingFile().getName());
+		File inputFile = new File(this.getOutputDir()+"/"+this.getProcessingFile().getName()+".temp");
+		StringBuilder sb = new StringBuilder();
+		List<String> lines = FileUtils.ReadLines(inputFile);
+		for(int i=0;i<lines.size();i++){
+			if(!lines.get(i).trim().isEmpty()){
+				sb.append(lines.get(i)+"\n");
+			}else if(i<lines.size()-1 && !(lines.get(i+1).trim().isEmpty())){
+				sb.append(lines.get(i)+"\n");
+			}
+		}
+		FileUtils.WriteFile(outputFile, sb);
+		FileUtils.Delete(inputFile, true);
 	}
 
 	private void writeToFile(String processedText,String fileName){
 		if(outputDir.exists() || outputDir.mkdirs()){ 
 			File preprocessDir = new File(outputDir+"/");
+			File processedFile = new File(preprocessDir+"/"+fileName+".temp");
 			if(!preprocessDir.exists())
+				preprocessDir.deleteOnExit();
 				preprocessDir.mkdir();
-			File processedFile = new File(preprocessDir+"/"+fileName);
 			FileUtils.AppendToFile(processedFile, processedText);
 		}else{
 			throw new RuntimeException("Couldn't create OP directory: "+outputDir.getAbsolutePath());
